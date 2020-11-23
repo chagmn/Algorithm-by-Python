c5514bd99af0c68331b96b205fb2922a128daf78
chagmn <sopg4@naver.com>
2020-11-23 18:01:24 +0900

    BAEKJOON Algorithm - Queue

---
+++ Basic/BJ_11866.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+
+n,k = map(int,sys.stdin.readline().split())
+queue = deque()
+answer = []
+
+for i in range(n):
+    queue.append(i)
+
+while len(arr) > 0:
+
