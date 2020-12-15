e40ce8af747d40a6b1934a20c97ccd405694f6bf
chagmn <sopg4@naver.com>
2020-12-15 23:42:45 +0900

    BAEKJOON Algorithm Study

--- Basic/BJ_11866.py
+++ Basic/BJ_11866.py
@@ -1,12 +1,17 @@
 import sys
 from collections import deque
 
-n,k = map(int,sys.stdin.readline().split())
+n, k = map(int, sys.stdin.readline().split())
 queue = deque()
+arr = []
 answer = []
 
 for i in range(n):
-    queue.append(i)
+    arr.append(i+1)
 
 while len(arr) > 0:
+    answer.append(arr[k])
+    arr.remove(k)
 
+    if k > len(arr):
+        k = 
