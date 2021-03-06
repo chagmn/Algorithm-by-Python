764891da0df41832958f0b986b9eb5a56dc2c427
chagmn <sopg4@naver.com>
2021-03-06 21:17:07 +0900

    Add 크레인 인형뽑기 Solving

--- Programmers/Level1/크레인 인형뽑기/solving.py
+++ Programmers/Level1/크레인 인형뽑기/solving.py
@@ -1,24 +1,24 @@
 def solution(board, moves):
     answer = 0
     select = []
-    
+
     for i in range(len(moves)):
         for j in range(len(board)):
-            
-            if board[j][moves[i]-1] != 0:
-                val = board[j][moves[i]-1]
-                
+
+            if board[j][moves[i] - 1] != 0:
+                val = board[j][moves[i] - 1]
+
                 if len(select) != 0:
-                    if select[len(select)-1] != val: # 이전에 선택한거와 동일한가?
+                    if select[len(select) - 1] != val:  # 이전에 선택한거와 동일한가?
                         select.append(val)  # 아니면 추가
-                        board[j][moves[i]-1] = 0
+                        board[j][moves[i] - 1] = 0
                     else:
-                        select.pop()        # 들어있는거 삭제
+                        select.pop()  # 들어있는거 값 삭제
                         answer += 2
-                        board[j][moves[i]-1] = 0
+                        board[j][moves[i] - 1] = 0
                 else:
                     select.append(val)
-                    board[j][moves[i]-1] = 0
+                    board[j][moves[i] - 1] = 0
                 break
-                
+
     return answer
