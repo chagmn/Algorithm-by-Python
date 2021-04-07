df939c064ce74bb00421ba70576c4c433f33397f
chagmn <sopg4@naver.com>
2021-04-07 12:32:33 +0900

    Add 2016년 Solving

---
+++ Programmers/Level2/조이스틱/solving.py
@@ -0,0 +1,21 @@
+import string
+
+
+def solution(name):
+    answer = 0
+    makeName = ""
+    index = 0
+
+    for i in range(len(name)):
+        makeName += "A"
+
+    while makeName != name:
+        
+
+    print(makeName)
+
+    return answer
+
+
+name = "JEROEN"
+print(solution(name))
