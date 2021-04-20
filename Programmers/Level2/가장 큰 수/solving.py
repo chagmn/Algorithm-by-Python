1702d1a0591b70ab6dc5bacef7c4e03dd1b97a22
chagmn <sopg4@naver.com>
2021-04-20 17:26:25 +0900

    Add 더 큰 숫자 Solving

--- Programmers/Level2/가장 큰 수/solving.py
+++ Programmers/Level2/가장 큰 수/solving.py
@@ -1,9 +1,9 @@
 def solution(numbers):
     answer = ""
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
+    numbers.sort(reverse=True)
     print(numbers)
-    return str("".join(numbers))
+    return str(int("".join(numbers)))
 
 
 print(solution([0, 0, 0]))
