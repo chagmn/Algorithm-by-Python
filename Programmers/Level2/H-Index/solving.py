813de4a2e8e1922461660b28d81b5aae6c43a0b5
chagmn <sopg4@naver.com>
2021-04-24 12:26:48 +0900

    Add H-Index Solving

---
+++ Programmers/Level2/H-Index/solving.py
@@ -0,0 +1,11 @@
+def solution(citations):
+    n = len(citations)
+    citations.sort()
+
+    for i in range(n):
+        if citations[i] >= n - i:
+            return n - i
+    return 0
+
+
+solution([3, 0, 6, 1, 5])
